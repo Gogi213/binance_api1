8bd18549bbc13c0de8fc514cd38f85310020af5d
Redmi <1993georgiy@gmail.com>
2023-07-08 17:53:38 +0300

    убрал праздник

--- venv/all files/main.py
+++ venv/all files/main.py
@@ -11,5 +11,3 @@ df.to_csv('C:\\Users\\Redmi\\PycharmProjects\\pythonProject1\\venv\\all files\\b
 
 
 print("Data has been written to 'binance_data.csv'")
-
-# праздник к нам приходиит всегда с кока кола
