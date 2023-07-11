a22b89bbf8175cfe52cde776d65a43c269fe11bf
Redmi <1993georgiy@gmail.com>
2023-07-11 14:55:34 +0300

    Процессинг добавил в гит

---
+++ venv/all files/data_processing.py
@@ -0,0 +1,28 @@
+# data_processing.py
+import pandas as pd
+
+def custom_format(x):
+    if isinstance(x, float):
+        str_x = format(x, '.15f')  # Convert number to fixed-point notation
+        if '.' in str_x:
+            decimal_part = str_x.split('.')[1]
+            num_decimals = len(decimal_part.rstrip('0'))  # Remove trailing zeros
+            return format(x, f'.{num_decimals}f')
+    return x
+
+
+def process_data(df):
+    # Get the list of column names
+    cols = list(df.columns)
+    # Move 'status' column right after 'symbol'
+    cols.insert(1, cols.pop(cols.index('status')))
+    # Reindex the DataFrame
+    df = df.reindex(columns=cols)
+    # Filter out rows with 'BREAK' status or NaN status
+    df = df[(df['status'] != 'BREAK') & (df['status'].notna())]
+    df['pair'].fillna(df['symbol'], inplace=True)
+    df[['base', 'quote']] = df['pair'].str.split('/', expand=True)
+    df[['askPrice', 'bidPrice', 'askQty', 'bidQty']] = df[['askPrice', 'bidPrice', 'askQty', 'bidQty']].astype(float)
+    df = df.loc[(df['bidPrice'] > 0) & (df['askPrice'] > 0)]
+    formatted_df = df.applymap(custom_format)
+    return formatted_df
