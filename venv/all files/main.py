d1b0e8dcecf7383d154c88217e407776ad9160bc
Redmi <1993georgiy@gmail.com>
2023-07-12 18:28:34 +0300

    Добавление обновления таблиц prices и data. Файл csv обновляется целиком. Таблица второго этапа не обновляется

--- venv/all files/main.py
+++ venv/all files/main.py
@@ -3,47 +3,63 @@ import pandas as pd
 import binance_api
 from binance_api import get_binance_data, get_binance_symbols, get_binance_status
 from calculate_profit import calculate_profit
-from calculate_profit2 import calculate_profit2  # Import new function
+from calculate_profit2 import calculate_profit2 # Import new function
 from data_processing import custom_format, process_data
+import psycopg2 # Import psycopg2 library
+
+# Establish a connection to the database
+def connect_to_db(database='binance_project', user='postgres', password='19938713', host='localhost', port='5432'):
+    conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    return conn
+
+# Function to update a table in the database
+def update_table(conn, df, table_name):
+    cursor = conn.cursor()
+    # Delete existing data from the table
+    cursor.execute(f'DELETE FROM {table_name}')
+    # Insert new data into the table
+    for index, row in df.iterrows():
+        cursor.execute(
+            f"""INSERT INTO {table_name} (symbol, status, bidprice, bidqty, askprice, askqty, pair, base, quote) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+            (row['symbol'], row['status'], row['bidprice'], row['bidqty'], row['askprice'], row['askqty'], row['pair'], row['base'], row['quote'])
+        )
+    # Commit the changes and close the cursor
+    conn.commit()
+    cursor.close()
 
 data = get_binance_data()
-# Convert list of dictionaries to DataFrame and rename columns
 df_data = pd.DataFrame(data)
 df_data = df_data.rename(columns={'bidPrice': 'bidprice', 'bidQty': 'bidqty', 'askPrice': 'askprice', 'askQty': 'askqty'})
 pairs = get_binance_symbols()
 status = get_binance_status()
 df = calculate_profit(df_data, pairs)
 df['status'] = df['symbol'].map(status)
-
-
 pairs = get_binance_symbols()
 status = get_binance_status()
 df = calculate_profit(data, pairs)
 df['status'] = df['symbol'].map(status)
-
-# Rename columns in df
 df = df.rename(columns={'bidPrice': 'bidprice', 'bidQty': 'bidqty', 'askPrice': 'askprice', 'askQty': 'askqty'})
-
-# Process the data
 formatted_df = process_data(df)
-formatted_df.to_csv('C:\\Users\\Redmi\\PycharmProjects\\pythonProject1\\venv\\all files\\binance_data.csv', index=False)
 
 df_prices = pd.DataFrame(data)
 df_prices['pair'] = df_prices['symbol'].map(pairs)
-
-# Rename columns in df_prices
 df_prices = df_prices.rename(columns={'bidPrice': 'bidprice', 'bidQty': 'bidqty', 'askPrice': 'askprice', 'askQty': 'askqty'})
-
 df_prices['status'] = df_prices['symbol'].map(status)
-
-# Process the prices data
 formatted_df_prices = process_data(df_prices)
-formatted_df_prices.to_csv('C:\\Users\\Redmi\\PycharmProjects\\pythonProject1\\venv\\all files\\binance_prices.csv', index=False)
+
+# Connect to the database
+conn = connect_to_db()
+
+# Update the 'binance_prices' and 'binance_data' tables
+update_table(conn, formatted_df_prices, 'binance_prices')
+update_table(conn, formatted_df, 'binance_data')
+
+# Close the connection to the database
+conn.close()
 
 print("Data has been written to 'binance_data.csv'")
 print("Data has been written to 'binance_prices.csv'")
 
 # Call new function for the second stage of profit calculation
 calculate_profit2()
-
 print("Second stage of profit calculation has been completed.")
