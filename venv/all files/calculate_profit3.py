96aa73a01773e1015ac513dd772bf599c5560e42
Redmi <1993georgiy@gmail.com>
2023-07-16 23:48:39 +0300

    ХЗ

--- venv/all files/calculate_profit3.py
+++ venv/all files/calculate_profit3.py
@@ -2,7 +2,6 @@
 import pandas as pd
 from bd import connect_to_db, update_table
 
-
 def calculate_profit3():
     # Load data from table
     conn = connect_to_db()
@@ -12,8 +11,8 @@ def calculate_profit3():
     # Define commission
     commission = 0.001
 
-    # Define stablecoins
-    stablecoins = ['USDT', 'USDC', 'BUSD', 'TUSD', 'DAI']
+    # Define stablecoin
+    stablecoin = 'USDT'
 
     # Iterate over each row in df_data
     for index, row in df_data.iterrows():
@@ -24,11 +23,11 @@ def calculate_profit3():
         if row['amount2'] is not None:
             quantity = float(row['amount2'])
         else:
-            print(f"Warning: amount2 is None for row {row}")
+            # print(f"Warning: amount2 is None for row {row}")
             quantity = 0.0  # or some other default value
 
-        # Filter df_prices for rows where the base currency is the swap currency and the quote currency is a stablecoin
-        df_prices_filtered = df_prices[(df_prices['base'] == swap_currency) & (df_prices['quote'].isin(stablecoins))]
+        # Filter df_prices for rows where the base currency is the swap currency and the quote currency is USDT
+        df_prices_filtered = df_prices[(df_prices['base'] == swap_currency) & (df_prices['quote'] == stablecoin)]
 
         # Initialize variables to store the best profit and corresponding row
         best_profit = 0
@@ -37,7 +36,6 @@ def calculate_profit3():
         # Iterate over each row in df_prices_filtered
         for index2, row2 in df_prices_filtered.iterrows():
             # Calculate the profit for this row
-            # If the base currency is the swap currency, we are selling
             amount3 = quantity / float(row2['bidprice']) * (1 - commission)
             usdt_equals3 = amount3 * float(row2['askprice'])
             profit3 = (usdt_equals3 - quantity) / quantity * 100
@@ -66,13 +64,13 @@ def calculate_profit3():
             df_data.loc[index, 'bidqty3'] = best_row['bidqty']
             df_data.loc[index, 'askprice3'] = best_row['askprice']
             df_data.loc[index, 'askqty3'] = best_row['askqty']
-            df_data.loc[index, 'swap3'] = best_row['quote']
+            df_data.loc[index, 'swap3'] = stablecoin
             df_data.loc[index, 'amount3'] = amount3
             df_data.loc[index, 'usdt_equals3'] = usdt_equals3
             df_data.loc[index, 'profit3'] = profit3
 
-    # Update table binance_data
-    update_table(conn, df_data, 'binance_data')
+        # Update table binance_data
+        update_table(conn, df_data, 'binance_data')
 
     # Connect to the database
     conn = connect_to_db()
